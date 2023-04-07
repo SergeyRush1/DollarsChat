04ad4dc259b9ef8bbd26d17282fd1f5c96d1e431
SergeyRush1 <ayty-ytyz@mail.ru>
2023-04-07 13:31:07 +0000

    Create test

---
+++ app/src/test/java/com/example/dollarschat/data/FirebaseHelperTest.kt
@@ -0,0 +1,15 @@
+package com.example.dollarschat.data
+
+import org.jetbrains.annotations.TestOnly
+import org.junit.Assert
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+
+class FirebaseHelperTest {
+    @Test
+    fun `auth Test`(){
+
+
+        Assertions.assertEquals(5,5)
+    }
+}
